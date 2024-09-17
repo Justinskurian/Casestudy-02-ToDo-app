a137a962d4ded3badbcf4b35b2d2517bd957b392
Justinskurian <justinskurian@gmail.com>
2024-09-17 20:04:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var container = document.getElementById('todo-container');
+const container=document.getElementById('todo-container');
 
-let count = 0;
-function complete() {
+let count=0;
+function complete(){
   return new Promise((resolve) => {
-    if (count == 5) {
+    if (count==5){
       resolve();
     }
   });
@@ -13,13 +13,13 @@ fetch('https://jsonplaceholder.typicode.com/todos')
   .then(response => response.json())
   .then(data => {
     data.forEach(data => {
-      const todoItem = document.createElement('div');
-      todoItem.classList.add('grid-item');
+      const todo=document.createElement('div');
+      todo.classList.add('grid-item');
 
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.addEventListener('change', () => {
-        if (checkbox.checked) {
+      const checkbox=document.createElement('input');
+      checkbox.type='checkbox';
+      checkbox.addEventListener('change',() => {
+        if(checkbox.checked){
           count++;
         }
 
@@ -27,11 +27,9 @@ complete()
 .then(() => alert('Congrats. 5 Tasks have been Successfully Completed'));
 });
 
-      todoItem.innerHTML = 
-      `<h5>${data.title}</h5>
-        <p> ${data.completed ? 'Completed' : 'Pending'}</p>`;
-        todoItem.appendChild(checkbox);
-      container.appendChild(todoItem);
+      todo.innerHTML=`<h5>${data.title}</h5> <p>${data.completed ? 'Completed':'Pending'}</p>`;
+      todo.appendChild(checkbox);
+      container.appendChild(todo);
     });
   })
-  .catch(error => console.error('Error:', error));
+  .catch(error => console.error('Error:',error));
